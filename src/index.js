6121d50f8da8b6f353dc3e63c65062585dc14a4d
Bruno Jullien <bruno.jullien@gmail.com>
2021-03-15 18:26:50 -0700

    3.2. Basics, 3.3. JSX

--- src/index.js
+++ src/index.js
@@ -6,3 +6,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
+
+/* 
+    NOTES:
+        - can even render a non-react: ReactDOM.render(<h1>Hello</h1>, document.getElementById('root'));
+        - generally there is only 1 root app element
+*/
